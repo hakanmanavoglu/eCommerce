1c9d9abe1232b27041b95e6974b2cdd3056357fd
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2014-02-05 14:42:04 +0200

    Added IOC (unity container)

---
+++ eCommerce.IOC/App_Start/UnityConfig.cs
@@ -0,0 +1,52 @@
+using eCommerce.Core.Domain.DbEntities;
+using eCommerce.Data.Repositories;
+using eCommerce.Data.UnitOfWork;
+using Microsoft.Practices.Unity;
+using System.Web.Mvc;
+using Unity.Mvc5;
+
+namespace eCommerce.IOC
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();
+            RegisterTypes(container);
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            container.BindInRequestScope<IGenericRepository<User>, GenericRepository<User>>();
+            container.BindInRequestScope<IGenericRepository<Role>, GenericRepository<Role>>();
+
+            container.BindInRequestScope<IUnitOfWork, UnitOfWork>();
+
+            //container.BindInRequestScope<IUserService, UserService>();
+            //container.BindInRequestScope<IRoleService, RoleService>();
+        }
+    }
+
+    /// <summary>
+    /// Bind the given interface in request scope
+    /// </summary>
+    public static class IOCExtensions
+    {
+        public static void BindInRequestScope<T1, T2>(this IUnityContainer container) where T2 : T1
+        {
+            container.RegisterType<T1, T2>(new HierarchicalLifetimeManager());
+        }
+
+        public static void BindInSingletonScope<T1, T2>(this IUnityContainer container) where T2 : T1
+        {
+            container.RegisterType<T1, T2>(new ContainerControlledLifetimeManager());
+        }
+    }
+}
