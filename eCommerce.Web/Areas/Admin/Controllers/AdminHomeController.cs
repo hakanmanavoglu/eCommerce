6df3d68e52261b29a8b37c410fd9dd3fc3a66dd0
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2014-02-07 17:58:53 +0200

    Modified admin layout page

---
+++ eCommerce.Web/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,20 @@
+﻿using eCommerce.Data.UnitOfWork;
+using eCommerce.Web.Framework.Controllers;
+using System.Web.Mvc;
+
+namespace eCommerce.Web.Areas.Admin.Controllers
+{
+    public class AdminHomeController : NoAuthorizedController
+    {
+        public AdminHomeController(IUnitOfWork uow)
+            : base(uow)
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ eCommerce.Web/Areas/Admin/Models/ProductModels/ProductViewModel.cs
@@ -0,0 +1,57 @@
+﻿using eCommerce.Core.Domain.DbEntities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace eCommerce.Web.Areas.Admin.Models.ProductModels
+{
+    public class ProductViewModel
+    {
+        [Required(ErrorMessage = "{0} alanı gereklidir!")]
+        [Display(Name = "Ürün Adı")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} alanı gereklidir!")]
+        [Display(Name = "Ürün Resim")]
+        public HttpPostedFileBase ProfileImgUrlOriginal { get; set; }
+
+        [Display(Name = "Seri No")]
+        public string SerialNo { get; set; }
+
+        [Required(ErrorMessage = "{0} alanı gereklidir!")]
+        [Display(Name = "Adet")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "{0} alanı gereklidir!")]
+        [Display(Name = "Kısa Açıklama")]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "Detaylı Açıklama")]
+        public string HtmlDescription { get; set; }
+
+        [Display(Name = "İndirimli?")]
+        public bool HasDiscount { get; set; }
+
+        [Display(Name = "İndirim Tutar")]
+        public float Discount { get; set; }
+
+        [Display(Name = "İndirim Yüzde")]
+        public float DiscountRate { get; set; }
+
+        [Display(Name = "Eski Fiyat")]
+        public float OldPrice { get; set; }
+
+        [Display(Name = "Yeni Fiyat")]
+        public float NewPrice { get; set; }
+
+        [Display(Name = "Para Birimi")]
+        public string PriceCurrency { get; set; }
+
+        [Required(ErrorMessage = "{0} alanı gereklidir!")]
+        [Display(Name = "Kategori")]
+        public int CategoryId { get; set; }
+
+        public virtual IEnumerable<string> PriceCurrencies { get; set; }
+        public virtual IEnumerable<Category> Categories { get; set; }
+    }
+}
